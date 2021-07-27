4df4daa193f41b3cee786d6a340bf698e0f55636
samkaff <78622572+samkaff@users.noreply.github.com>
2021-07-26 19:23:13 -0700

    Update nmap.py

--- nmap.py
+++ nmap.py
@@ -1,29 +1,16 @@
 #! /usr/bin/env python3
 
-import sys
-import re
 import nmap
-import nmapthon as scan
 
+nmScan = nmap.PortScanner()
+nmScan.scan('10.0.0.94', '22')
+nmScan.command_line()
+for host in nmScan.all_hosts():
+    print(host) 
+    for proto in nmScan[host].all_protocols():
+        lport = nmScan[host][proto].keys()
+        for port in lport:
+            print (port)
+            print(nmScan[host][proto][port]['state'])
 
-scanner = scan.NmapScanner('10.0.0.0/24', ports=22)
-
-try:
-    scanner.run()
-except scan.NmapScanError as error:
-   print('Error!')
-
-for host in scanner.scanned_hosts():
-   print("Hostname: {}".format(','.join(scan.hostnames(host))))
-
-#print("Start Timestamp: {} ".format(scanner.start_timestamp))
-print("Start Time: {} ".format(scanner.start_time))
-print("Exit Status: {} ".format(scanner.exit_status))
-print("Arguments: {} ".format(scanner.args))
-print("Summary: {} ".format(scanner.summary))
-#print("Version: {} ".format(scanner.version))
-#print("End Timestamp: {} ".format(scanner.end_timestamp))
-print("End Time: {} ".format(scanner.end_time))
-print("Finished? {} ".format(scanner.finished))
-print("Tolerant Errors: {} ".format(scanner.tolerant_errors))
 
