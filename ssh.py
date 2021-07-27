ea329107be2993b69ab1833343e7f9daa4a8b449
Devon Meadows <78694622+devonmeadows@users.noreply.github.com>
2021-07-26 20:39:55 -0500

    Create ssh.py

---
+++ ssh.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import paramiko
+# paramiko = python implementation of SSHv2 protocol; documentation @ docs.paramiko.org
+# references = https://www.kite.com/python/answers/how-to-ssh-into-a-server-in-python & https://support.sciencelogic.com/s/article/1440
+
+# set variables to be interpreted by paramiko sshclient (host, port, username, password)
+host = ""
+port = 22
+username = ""
+password = ""
+
+#define command variable
+command = "cd ~/Desktop; touch 'WormyWasHere'"
+
+# define client/ssh variable to call sshclient
+ssh = paramiko.SSHClient()
+# set missing host key policy to allow script to SSH into a serve w/ unknown host keys; reference AutoAddPolicy to add above variables when calling SSH
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# connect to client
+ssh.connect(host, port, username, password)
+
+# execute command & return 3 tuple (stdin, stdout, stderr)
+stdin, stdout, stderr = ssh.exec_command(command)
+#return stdout
+for lines in stdout: 
+    print(line)
